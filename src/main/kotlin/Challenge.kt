b733450c33e00c5800f965b1735c3f6d9ddb241b
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-11-30 19:35:15 +0100

    updated

--- src/main/kotlin/Challenge.kt
+++ src/main/kotlin/Challenge.kt
@@ -1,2 +1,13 @@
-class Challenge {
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+abstract class Challenge(
+    val name: String? = null
+) {
+    protected val input = File(javaClass.getResource("input").path).readText()
+
+    abstract fun part1(): Any?
+    abstract fun part2(): Any?
+
+    fun solve() = measureTimeMillis { listOf(name, part1(), part2()) }
 }
--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,4 +1,18 @@
 package day1
 
-object Day1 {
+import Challenge
+
+fun main(){
+    println(Day1.part1())
+    println(Day1.part2())
+}
+
+object Day1 : Challenge() {
+    override fun part1(): Any? {
+        return input.lines()[0]
+    }
+
+    override fun part2(): Any? {
+        return input.lines()[1]
+    }
 }
