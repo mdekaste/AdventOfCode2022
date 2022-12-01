9b0c1ae1bfad9a324882c48a17b5f7907174f0bd
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-12-01 06:43:08 +0100

    day 2 prep

---
+++ src/main/kotlin/day2/Day2.kt
@@ -0,0 +1,18 @@
+package day2
+
+import Challenge
+
+fun main(){
+    println(Day2.part1())
+    println(Day2.part2())
+}
+
+object Day2 : Challenge() {
+    override fun part1(): Any? {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): Any? {
+        TODO("Not yet implemented")
+    }
+}
