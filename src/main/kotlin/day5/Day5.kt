03e347552337b4f16579427082e35c48f9029e64
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-12-05 06:57:31 +0100

    day5 "mooi" gemaakt

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -8,11 +8,34 @@ fun main(){
 }
 
 object Day5 : Challenge() {
-    override fun part1(): Any? {
-        return "part1"
+    val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
+    val parsed = input.split(System.lineSeparator() + System.lineSeparator()).let { (a,b) ->
+        List(9){ mutableListOf<Char>() }.apply {
+            a.lines().forEach { line ->
+                for((index, value) in line.withIndex()){
+                    if(value.isUpperCase()){
+                        this[(index - 1) / 4].add(0, value)
+                    }
+                }
+            }
+        } to b.lines().mapNotNull { line ->
+            regex.matchEntire(line)?.destructured?.toList()?.map(String::toInt)
+        }
     }
 
-    override fun part2(): Any? {
-        return "part2"
+    override fun part1() = craneGame(parsed.first.map(List<Char>::toMutableList), parsed.second, false)
+
+    override fun part2() = craneGame(parsed.first.map(List<Char>::toMutableList), parsed.second, true)
+
+    private fun craneGame(stacks: List<MutableList<Char>>, moves: List<List<Int>>, reverse: Boolean): String {
+        moves.forEach { (amount, from, to) ->
+            stacks[to - 1].addAll(
+                grabTop(stacks[from - 1], amount, reverse)
+            )
+        }
+        return stacks.map(List<*>::last).joinToString(separator = "")
     }
+    private fun grabTop(toRemoveFrom: MutableList<Char>, amount: Int, reverse: Boolean) =
+        MutableList(amount){ toRemoveFrom.removeLast() }
+            .apply { if(reverse) reverse() }
 }
