9b4703c11296788095a06acb42ddb95a3b568fa1
mdekaste <m.dekaste@hotmail.com>
2022-12-05 12:28:02 +0100

    added extensions for future work

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -1,8 +1,11 @@
 package day5
 
 import Challenge
+import extractInts
+import removeLast
+import splitOnEmpty
 
-fun main(){
+fun main() {
     Day5.part1().let(::println)
     Day5.part2().let(::println)
 }
@@ -11,30 +14,27 @@ object Day5 : Challenge() {
     val stacks: List<List<Char>>
     val moves: List<List<Int>>
     init {
-        val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
-        input.split(System.lineSeparator() + System.lineSeparator()).let { (a,b) ->
-            stacks = List(9){ mutableListOf<Char>() }.apply {
+        input.splitOnEmpty().let { (a, b) ->
+            stacks = List(9) { mutableListOf<Char>() }.apply {
                 a.lines().forEach { line ->
-                    for((index, value) in line.withIndex()){
-                        if(value.isUpperCase()){
+                    for ((index, value) in line.withIndex()) {
+                        if (value.isUpperCase()) {
                             this[(index - 1) / 4].add(0, value)
                         }
                     }
                 }
             }
-            moves = b.lines().mapNotNull { line ->
-                regex.matchEntire(line)?.destructured?.toList()?.map(String::toInt)
-            }
+            moves = b.lines().map(String::extractInts)
         }
     }
 
-    override fun part1() = craneGame(reverse = false)
+    override fun part1() = craneGame(reverse = true)
 
-    override fun part2() = craneGame(reverse = true)
+    override fun part2() = craneGame(reverse = false)
 
     private fun craneGame(reverse: Boolean) = stacks.map(List<Char>::toMutableList).apply {
         for ((amount, from, to) in moves) {
-            this[to - 1] += MutableList(amount) { this[from - 1].removeLast() }.apply { if (reverse) reverse() }
+            this[to - 1] += this[from - 1].removeLast(amount).apply { if (reverse) reverse() }
         }
-    }.joinToString(separator = ""){ it.last().toString() }
+    }.joinToString(separator = "") { it.last().toString() }
 }
