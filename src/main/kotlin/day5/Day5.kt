4a5b6d10983941580e89283cae02b7e051390ae1
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-12-05 07:11:28 +0100

    day5 opgeschoond

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -8,29 +8,39 @@ fun main(){
 }
 
 object Day5 : Challenge() {
-    val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
-    val parsed = input.split(System.lineSeparator() + System.lineSeparator()).let { (a,b) ->
-        List(9){ mutableListOf<Char>() }.apply {
-            a.lines().forEach { line ->
-                for((index, value) in line.withIndex()){
-                    if(value.isUpperCase()){
-                        this[(index - 1) / 4].add(0, value)
+    val stacks: List<List<Char>>
+    val moves: List<List<Int>>
+    init {
+        val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
+        input.split(System.lineSeparator() + System.lineSeparator()).let { (a,b) ->
+            stacks = List(9){ mutableListOf<Char>() }.apply {
+                a.lines().forEach { line ->
+                    for((index, value) in line.withIndex()){
+                        if(value.isUpperCase()){
+                            this[(index - 1) / 4].add(0, value)
+                        }
                     }
                 }
             }
-        } to b.lines().mapNotNull { line ->
-            regex.matchEntire(line)?.destructured?.toList()?.map(String::toInt)
+            moves = b.lines().mapNotNull { line ->
+                regex.matchEntire(line)?.destructured?.toList()?.map(String::toInt)
+            }
         }
     }
 
-    override fun part1() = craneGame(parsed.first.map(List<Char>::toMutableList), parsed.second, false)
+    override fun part1() = craneGame(reverse = false)
 
-    override fun part2() = craneGame(parsed.first.map(List<Char>::toMutableList), parsed.second, true)
+    override fun part2() = craneGame(reverse = true)
 
-    private fun craneGame(stacks: List<MutableList<Char>>, moves: List<List<Int>>, reverse: Boolean): String {
-        moves.forEach { (amount, from, to) ->
+    private fun craneGame(reverse: Boolean): String {
+        val stacks = stacks.map(List<Char>::toMutableList)
+        for((amount, from, to) in moves){
             stacks[to - 1].addAll(
-                grabTop(stacks[from - 1], amount, reverse)
+                grabTop(
+                    toRemoveFrom = stacks[from - 1],
+                    amount = amount,
+                    reverse = reverse
+                )
             )
         }
         return stacks.map(List<*>::last).joinToString(separator = "")
