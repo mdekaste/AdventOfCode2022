df2128fbc895dd7bd1b5d79155ac7e907fe6514b
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-12-06 06:17:38 +0100

    day6 boring

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -8,11 +8,8 @@ fun main(){
 }
 
 object Day6 : Challenge() {
-    override fun part1(): Any? {
-        return "part1"
-    }
-
-    override fun part2(): Any? {
-        return "part2"
-    }
+    override fun part1() = solve(4)
+    override fun part2() = solve(14)
+    private fun solve(size: Int) = input.windowed(size).indexOfFirst { it.toSet().size == size } + size
 }
+
