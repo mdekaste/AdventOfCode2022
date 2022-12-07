a38622c436969f7b2d7d6121a860795c2aaf911c
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-12-07 08:27:57 +0100

    day 7 frustraties

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -2,17 +2,47 @@ package day7
 
 import Challenge
 
-fun main(){
+fun main() {
     Day7.part1().let(::println)
     Day7.part2().let(::println)
 }
 
 object Day7 : Challenge() {
-    override fun part1(): Any? {
-        return "part1"
+    private val root = Directory().also { root ->
+        input.lineSequence()
+            .map { it.split(" ") + "" } // Why no destructuring to nullable?
+            .fold(root) { directory, (a, b, c) ->
+                when {
+                    a == "$" && b == "cd" && c == "/" -> root
+                    a == "$" && b == "cd" && c == ".." -> directory.parent!!
+                    a == "$" && b == "cd" -> directory.directories.getValue(c)
+                    a == "$" && b == "ls" -> directory
+                    a == "dir" -> directory.apply { directories[b] = Directory(directory) }
+                    else -> directory.apply { updateDirectory(a.toInt()) }
+                }
+            }
     }
+    override fun part1() = root.getAllDirectories()
+        .map { it.fileSize }
+        .filter { it <= 100000 }
+        .sum()
 
-    override fun part2(): Any? {
-        return "part2"
+    override fun part2() = root.getAllDirectories()
+        .map { it.fileSize }
+        .filter { it >= root.fileSize - 40000000 }
+        .minOrNull()
+}
+
+class Directory(
+    val parent: Directory? = null,
+    val directories: MutableMap<String, Directory> = mutableMapOf(),
+    var fileSize: Long = 0
+) {
+    fun getAllDirectories(): Sequence<Directory> = sequenceOf(this) +
+        directories.values.flatMap(Directory::getAllDirectories)
+
+    fun updateDirectory(sizeOfFile: Int) {
+        fileSize += sizeOfFile
+        parent?.updateDirectory(sizeOfFile)
     }
 }
