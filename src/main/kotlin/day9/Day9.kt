fc819a117fd8edbac20ae8e8859336a8b495c9da
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-12-09 07:36:41 +0100

    made Point a dataclass instead for better readability

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -10,32 +10,32 @@ fun main() {
     Day9.part2().let(::println)
 }
 
-typealias Point = Pair<Int, Int>
+data class Point(val y: Int, val x: Int)
 object Day9 : Challenge() {
     val parsed = input
         .lineSequence()
         .map { it.split(" ").let { (a, b) -> a to b.toInt() } }
         .flatMap { (move, amount) -> generateSequence { move }.take(amount) }
         .map(::moveToDif)
-        .runningFold(List(10) { 0 to 0 }, ::moveRope)
+        .runningFold(List(10) { Point(0, 0) }, ::moveRope)
 
     private fun moveToDif(move: String) = when (move) {
-        "U" -> -1 to 0
-        "R" -> 0 to 1
-        "D" -> 1 to 0
-        "L" -> 0 to -1
+        "U" -> Point(-1, 0)
+        "R" -> Point(0, 1)
+        "D" -> Point(1, 0)
+        "L" -> Point(0, -1)
         else -> error("unknown move")
     }
 
     private fun moveRope(rope: List<Point>, direction: Point): List<Point> = rope
         .drop(1)
-        .runningFold(rope.first().first + direction.first to rope.first().second + direction.second, ::fixTail)
+        .runningFold(Point(rope.first().y + direction.y, rope.first().x + direction.x), ::fixTail)
 
     private fun fixTail(head: Point, tail: Point): Point {
-        val yDif = head.first - tail.first
-        val xDif = head.second - tail.second
+        val yDif = head.y - tail.y
+        val xDif = head.x - tail.x
         return when (max(yDif.absoluteValue, xDif.absoluteValue)) {
-            2 -> tail.first + yDif.sign to tail.second + xDif.sign
+            2 -> Point(tail.y + yDif.sign, tail.x + xDif.sign)
             else -> tail
         }
     }
