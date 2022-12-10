f246094c25cb29bb9717c2ea73870ed1e155e247
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-12-10 07:11:18 +0100

    day10

---
+++ src/main/kotlin/day10/Day10.kt
@@ -0,0 +1,28 @@
+package day10
+
+import Challenge
+
+fun main() {
+    Day10.part1().let(::println)
+    Day10.part2().let(::println)
+}
+
+object Day10 : Challenge() {
+    private val parsed = input.lines()
+        .flatMap {
+            when (val split = it.split(" ")) {
+                listOf("noop") -> listOf(0)
+                else -> listOf(0, split[1].toInt())
+            }
+        }.runningFold(1, Int::plus).dropLast(1)
+
+    override fun part1() = (20..220 step 40).sumOf { it * parsed[it - 1] }
+
+    override fun part2() = parsed
+        .chunked(40)
+        .joinToString("\n") {
+            it.withIndex().joinToString("") { (index, value) ->
+                if (value in setOf(index - 1, index, index + 1)) "#" else "."
+            }
+        }
+}
