3d96362d36ca6625b75f1e0560e116b1cfb0b900
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2022-12-11 10:35:25 +0100

    day11 some more pretty

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -8,48 +8,48 @@ fun main() {
 }
 
 object Day11 : Challenge() {
+
     class Monkey(
+        val initialItems: List<Long>,
         val inspection: (Long) -> Long,
         val testNumber: Long,
-        private val ifTrue: Int,
-        private val ifFalse: Int
-    ) {
-        fun throwTarget(number: Long) = if (number % testNumber == 0L) ifTrue else ifFalse
-    }
+        val ifTrue: Int,
+        val ifFalse: Int
+    )
 
     val parsed = input.split(System.lineSeparator() + System.lineSeparator()).map {
         it.lines().drop(1).let { (a, b, c, d, e) ->
             Monkey(
+                initialItems = a.substringAfter(": ").split(", ").map(String::toLong),
                 inspection = b.substringAfter("old ").split(" ")
                     .let { (type, value) -> (type == "+") to value.toLongOrNull() }
                     .let { (isAdd, long) -> if (isAdd) { x -> x + (long ?: x) } else { x -> x * (long ?: x) } },
                 testNumber = c.substringAfter("by ").toLong(),
                 ifTrue = d.substringAfter("monkey ").toInt(),
                 ifFalse = e.substringAfter("monkey ").toInt()
-            ) to a.substringAfter(": ").split(", ").map(String::toLong)
+            )
         }
-    }.unzip()
-
-    private val monkeys = parsed.first
-    private val initialItems = parsed.second
+    }
 
     private fun solve(times: Int, worryMitigation: (Long) -> Long): Long {
-        val inspections = LongArray(monkeys.size)
-        val itemsToTrack = initialItems.map { it.toMutableList() }
+        val inspections = IntArray(parsed.size)
+        val itemsToTrack = parsed.map { it.initialItems.toMutableList() }
         repeat(times) {
-            for ((index, monkey) in monkeys.withIndex()) {
-                inspections[index] = inspections[index] + itemsToTrack[index].size
-                for (item in itemsToTrack[index]) {
-                    val monkeyDoInspection = worryMitigation(monkey.inspection(item))
-                    itemsToTrack[monkey.throwTarget(monkeyDoInspection)] += monkeyDoInspection
+            for ((index, monkey) in parsed.withIndex()) {
+                val items = itemsToTrack[index]
+                inspections[index] += items.size
+                for (item in items) {
+                    val worry = worryMitigation(monkey.inspection(item))
+                    val target = if (worry % monkey.testNumber == 0L) monkey.ifTrue else monkey.ifFalse
+                    itemsToTrack[target] += worry
                 }
                 itemsToTrack[index].clear()
             }
         }
-        return inspections.sortedDescending().let { (a, b) -> a * b }
+        return inspections.sortedDescending().let { (a, b) -> 1L * a * b }
     }
 
     override fun part1() = solve(20) { it / 3 }
 
-    override fun part2() = solve(10_000) { it % monkeys.map(Monkey::testNumber).reduce(Long::times) }
+    override fun part2() = solve(10_000) { it % parsed.map(Monkey::testNumber).reduce(Long::times) }
 }
